4932485def62ae02b49f848c15158ce67764c67e
Annetta-Agustín <82288803+Agustin-Annetta@users.noreply.github.com>
2021-05-06 14:19:36 -0300

    Update EjemplosTipos.cpp

--- 01-EjemploTipos/EjemplosTipos.cpp
+++ 01-EjemploTipos/EjemplosTipos.cpp
@@ -1,1 +1,21 @@
 
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+int main()
+{
+	int pruebaUno = 1;
+	assert(pruebaUno == 1);
+	double pruebaDos = 2.5;
+	assert(pruebaDos == 2.5);
+	unsigned int pruebaTres = -3;
+	assert(pruebaTres == -3);
+	char pruebaCuatro = 'A';
+	assert(pruebaCuatro == 'A');
+	string pruebaCinco = "A";
+	assert(pruebaCinco == "A");
+	bool pruebaSeis = true;
+	assert(pruebaSeis == true);
+
+}
