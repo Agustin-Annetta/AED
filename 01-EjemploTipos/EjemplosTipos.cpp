9c90b357e8d5d5b994b2bd60fc396ff83ba20df8
Annetta-Agustín <82288803+Agustin-Annetta@users.noreply.github.com>
2021-05-06 14:29:24 -0300

    Update EjemplosTipos.cpp

--- 01-EjemploTipos/EjemplosTipos.cpp
+++ 01-EjemploTipos/EjemplosTipos.cpp
@@ -10,6 +10,7 @@ int main()
 	double pruebaDos = 2.5;
 	assert(pruebaDos == 2.5);
 	unsigned int pruebaTres = -3;
+	//assert(pruebaTres == 2);   aca salta un error y se detiene el programa
 	assert(pruebaTres == -3);
 	char pruebaCuatro = 'A';
 	assert(pruebaCuatro == 'A');
